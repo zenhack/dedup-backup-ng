47bd194c3577f65488a60e38235a75666ce78215
Ian Denhardt <ian@zenhack.net>
2017-12-09 18:00:31 -0500

    Add quickcheck test that catches our known bug.

--- tests/TestMain.hs
+++ tests/TestMain.hs
@@ -2,14 +2,39 @@ module Main where
 
 import DedupBackupNG hiding (main)
 
-import Control.Monad (when)
-import System.Exit   (exitFailure)
+import Control.Monad                        (replicateM)
+import System.Unix.Directory                (withTemporaryDirectory)
+import Test.Framework                       (defaultMain)
+import Test.Framework.Providers.QuickCheck2 (testProperty)
+import Test.QuickCheck                      (Gen, Property, arbitrary, choose)
+import Test.QuickCheck.Monadic              (monadicIO, pick, run)
+
+import qualified Data.ByteString as B
+
+-- Generate a bytestring up to 4 MiB in size. This is big enough to test
+-- deep-ish trees without being prohibitive.
+genBlob :: Gen B.ByteString
+genBlob = do
+    numBytes <- choose (0 :: Int, 4 * 1024 * 1024)
+    B.pack <$> replicateM numBytes arbitrary
+
+prop_saveRestoreBlob = monadicIO $ do
+    blob <- pick genBlob
+    -- run $ print (LB.length blob)
+    run $ withTemporaryDirectory "/tmp/store.XXXXXX" $ \path -> do
+        let oldPath = path ++ "/old"
+            newPath = path ++ "/new"
+        B.writeFile oldPath blob
+        store <- initStore (path ++ "/store")
+        ref <- storeFile store oldPath
+        extractFile store ref newPath
+        old <- B.readFile oldPath
+        new <- B.readFile newPath
+        return (old == new)
 
 main :: IO ()
-main = do
-    store <- initStore "/tmp/test-ddb-ng-store"
-    ref <- storeFile store "./LICENSE"
-    extractFile store ref "/tmp/LICENSE-test-ddb-ng"
-    old <- readFile "./LICENSE"
-    new <- readFile "/tmp/LICENSE-test-ddb-ng"
-    when (old /= new) $ exitFailure
+main = defaultMain
+    [ testProperty
+        "saving and then restoring a blob produces the same bytes."
+        prop_saveRestoreBlob
+    ]
