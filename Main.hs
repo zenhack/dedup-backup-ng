835dac673a50d4c235150f98f86d2c528395793c
Ian Denhardt <ian@zenhack.net>
2017-12-09 02:00:56 -0500

    Save & load blobs.
    Broken for trees of depth > 2; on load we get an error about a missing
    block.

--- Main.hs
+++ Main.hs
@@ -18,27 +18,37 @@
 --   one-piece.
 -- * A @blob@ is a logically contiguous sequence of bytes (e.g. the contents of
 --   a file), which may be stored in one or more blocks.
+--
+-- A blob is stored as a tree of blocks, where the leaves are the logical bytes
+-- of the blob itself, and the interior nodes are the concatenation of the
+-- sha256 hashes of their children.
+--
+-- The blocks do not store any other metadata; reading back a blob requires
+-- knowing the hash of the root block of its tree, and the depth of the tree.
+{-# LANGUAGE Rank2Types #-}
 module Main where
 
 import qualified Crypto.Hash.SHA256 as SHA256
 
 import Conduit
-    (ConduitM, Source, await, mapC, mapM_C, runConduit, yield, (.|))
-import Control.Exception   (bracket, catch, throwIO)
-import Control.Monad       (forM_, unless, when)
-import Control.Monad.Trans (lift)
-import Data.Monoid         ((<>))
-import System.Directory    (createDirectoryIfMissing)
-import System.IO           (Handle, IOMode(ReadMode), hClose, openBinaryFile)
-import System.IO.Error     (isAlreadyExistsError)
-import Text.Printf         (printf)
+import Control.Exception      (bracket, catch, throwIO)
+import Control.Monad          (forM_, unless, when)
+import Control.Monad.IO.Class (MonadIO(liftIO))
+import Data.Int               (Int64)
+import Data.Monoid            ((<>))
+import System.Directory       (createDirectoryIfMissing)
+import System.IO              (Handle, IOMode(ReadMode), hClose, openBinaryFile)
+import System.IO.Error        (isAlreadyExistsError)
+import Text.Printf            (printf)
 
 import qualified Data.ByteString         as B
 import qualified Data.ByteString.Base16  as Base16
 import qualified Data.ByteString.Builder as Builder
 import qualified Data.ByteString.Char8   as B8
 import qualified Data.ByteString.Lazy    as LBS
+import qualified System.Posix.Files      as P
 import qualified System.Posix.IO         as P
+import qualified System.Posix.Types      as P
 
 -- | Wrapper around sha256 digests; just introduces a bit of type safety.
 newtype Hash = Hash B.ByteString
@@ -48,13 +58,20 @@ newtype Hash = Hash B.ByteString
 newtype Store = Store FilePath
     deriving(Show, Eq)
 
+-- | A reference to a blob. This includes all information necessary to read the
+-- blob, not counting the location of the store.
+data BlobRef = BlobRef !Int64 !Hash
+    deriving(Show, Eq)
+
 -- | A block together with its hash.
 data HashedBlock = HashedBlock !Hash !B.ByteString
 
 -- | The maximum block size to store.
+blockSize :: Integral a => a
 blockSize = 4096
 
 -- | The size of a hash in bytes.
+hashSize :: Integral a => a
 hashSize = 256 `div` 8
 
 -- | Compute the hash of a block.
@@ -87,9 +104,9 @@ blockPath (Store storePath) (Hash digest) =
 
 -- | @'saveBlock' store block@ Saves @block@ to the store. If the block
 -- is already present, this is a no-op.
-saveBlock :: Store -> HashedBlock -> IO ()
+saveBlock :: MonadIO m => Store -> HashedBlock -> m ()
 saveBlock store (HashedBlock digest bytes) =
-    saveFile (blockPath store digest) bytes
+    liftIO $ saveFile (blockPath store digest) bytes
 
 -- @'loadBlock@ store digest@ returns the block corresponding to the
 -- given sha256 hash from @store@.
@@ -100,56 +117,115 @@ loadBlock store digest = B.readFile (blockPath store digest)
 --
 -- TODO: there's probably a library function that does this; we should
 -- look for for that and use it instead.
-hBlocks :: Handle -> Source IO B.ByteString
+hBlocks :: MonadIO m => Handle -> Source m B.ByteString
 hBlocks handle = do
-    block <- lift $ B.hGet handle blockSize
+    block <- liftIO $ B.hGet handle blockSize
     when (block /= B.empty) $ do
         yield block
         hBlocks handle
 
--- | Save all blocks read from the handle to the store. TODO: change this so
--- it also stores & returns enough info to reconstruct the blob.
-saveBlob :: Store -> Handle -> IO ()
-saveBlob store h = runConduit $
-    hBlocks h .|
-    mapC hash .|
-    buildNodes .|
-    mapM_C (saveBlock store)
+-- | @'saveBlob' store size handle@ saves all blocks read from the handle
+-- to the store, expecting @size@ bytes total. 'saveBlob' will not read
+-- more blocks than indicated by @size@ (though it may read past the exact
+-- byte index, if there are more bytes available in the same block).
+--
+-- Returns a BlobRef referencing the blob.
+--
+-- TODO: somehow issue a warning if the blob size is shorter than the
+-- available bytes.
+saveBlob :: Store -> Int64 -> Handle -> IO BlobRef
+saveBlob store size h = do
+    let treeDepth = depthForSize size
+        numBlocks = blocksForSize size
+    Just (HashedBlock digest _) <- runConduit $
+        hBlocks h .|
+        takeC numBlocks .|
+        mapC hash .|
+        buildTree treeDepth .|
+        saveBlocks store .|
+        lastC
+    return $ BlobRef treeDepth digest
 
--- | Build the interior nodes of our B-tree. The input stream should be the
--- leaf blocks of a blob, the output stream will be all of the blocks needed to
--- be stored.
+-- Load a blob from disk.
+loadBlob :: MonadIO m => Store -> BlobRef -> Producer m B.ByteString
+loadBlob store (BlobRef 1 digest) =
+    liftIO (loadBlock store digest) >>= yield
+loadBlob store (BlobRef n digest) = do
+    block <- liftIO $ loadBlock store digest
+    forM_ (chunk block) $ \digest' -> do
+        loadBlob store (BlobRef (n-1) (Hash digest'))
+  where
+    chunk bs
+        | bs == B.empty = []
+        | otherwise = B.take hashSize bs : chunk (B.drop hashSize bs)
+
+-- | Save all blocks in the stream to the store, and pass them along.
+saveBlocks :: MonadIO m => Store -> ConduitM HashedBlock HashedBlock m ()
+saveBlocks store = iterMC (saveBlock store)
+
+-- | @'depthForSize' n@ computes the depth of a tree for a blob of size @n@.
+depthForSize :: Int64 -> Int64
+depthForSize 0 = 0
+depthForSize n
+    | n <= blockSize = 1
+    | otherwise = 1 + depthForSize (n `div` branchFactor)
+  where
+    branchFactor = blockSize `div` hashSize
+
+blocksForSize :: Int64 -> Int
+blocksForSize size = fromIntegral $
+    let wholeBlocks = size `div` blockSize
+        haveRemainder = size `mod` blockSize /= 0
+    in if haveRemainder
+        then wholeBlocks + 1
+        else wholeBlocks
+
+-- | @'buildTree' size@ builds the tree for a blob of size @size@ bytes. It
+-- expects the raw (leaf) blocks of the blob to be its input, and yields the tree
+-- in post-order (i.e. each interior node is yielded after its children).
+buildTree :: MonadIO m => Int64 -> ConduitM HashedBlock HashedBlock m ()
+-- We should only hit the first case if the blob is empty; otherwise we won't
+-- recurse down that far.
+buildTree 0 = yield $ hash $ B8.pack ""
+buildTree 1 = mapC id
+buildTree n = buildNodes .| buildTree (n-1)
+
+
+-- | Build interior nodes from the incoming stream. The incoming blocks are
+-- copied to the output, and interior nodes are injected after their contents.
 --
--- TODO:
---
--- * describe the scheme in more detail
--- * find a way to report the hash of the tree's root.
--- * I suspect this doesn't work with trees of depth > 2.
+-- Note that this only generates one layer of nodes; see 'buildTree'.
 buildNodes :: Monad m => ConduitM HashedBlock HashedBlock m ()
 buildNodes = go 0 mempty where
-    go :: Monad m => Int -> Builder.Builder -> ConduitM HashedBlock HashedBlock m ()
-    go n hashes
-        | (n + hashSize) > blockSize = do
-            yield $ buildHashes hashes
+    go :: Monad m => Int64 -> Builder.Builder -> ConduitM HashedBlock HashedBlock m ()
+    go bufSize buf
+        | (bufSize + hashSize) > blockSize = do
+            -- adding another hash would overflow the block; yield what we've
+            -- got and start the next block.
+            yield $ buildHashes buf
             go 0 mempty
         | otherwise = do
             item <- await
             case item of
-                Just block@(HashedBlock (Hash digest) bytes) -> do
+                Just block@(HashedBlock (Hash digest) _) -> do
                     yield block
-                    go (n + hashSize) (hashes <> Builder.byteString digest)
+                    go (bufSize + hashSize) (buf <> Builder.byteString digest)
                 Nothing ->
-                    when (n > 1) $
-                        yield $ buildHashes hashes
+                    yield $ buildHashes buf
     buildHashes = hash . LBS.toStrict . Builder.toLazyByteString
 
--- | Placeholder during experimentation. This stores each of the blocks of
--- the named file in the store at /tmp/bar.
-doIt :: FilePath -> IO ()
-doIt filename = bracket
-    (openBinaryFile filename ReadMode)
-    hClose
-    (saveBlob $ Store "/tmp/bar")
+storeFile :: Store -> FilePath -> IO BlobRef
+storeFile store filename = do
+    status <- P.getFileStatus filename
+    let P.COff size = P.fileSize status
+    bracket
+        (openBinaryFile filename ReadMode)
+        hClose
+        (\h -> saveBlob store size h)
+
+extractFile :: Store -> BlobRef -> FilePath -> IO ()
+extractFile store ref path = runConduitRes $
+    loadBlob store ref .| sinkFileBS path
 
 -- | @'initStore' dir@ creates the directory structure necessary for
 -- storage in the directory @dir@.
