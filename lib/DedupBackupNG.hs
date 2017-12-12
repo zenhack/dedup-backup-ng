6751f6c1e4b14eedec10a9bb900f91e4bdc51465
Ian Denhardt <ian@zenhack.net>
2017-12-12 03:06:35 -0500

    Clean up the type class's presentation a bit.
    Not structurally any different, but better documented/laid out.

--- lib/DedupBackupNG.hs
+++ lib/DedupBackupNG.hs
@@ -120,47 +120,73 @@ hashSize = 256 `div` 8
 hash :: Block -> HashedBlock
 hash block@(Block bytes) = HashedBlock (Hash (SHA256.hash bytes)) block
 
+-- Type class to abstract out the filesystem related operations we need, so
+-- we can spoof them in the tests. Most of these are equivalent to some
+-- existing function in IO, by the same name (or similar where noted).
 class MonadMask m => MonadFileSystem m where
     type Handle m
     type FileStatus m
 
-    -- Like 'B.hGet'
+    -- 'B.hGet'
     hGetBS :: Handle m -> Int -> m B.ByteString
 
-    -- Like theh corresponding function from 'System.IO'
     hClose :: Handle m -> m ()
-
-    -- Like 'System.IO.openBinaryFile'
     openBinaryFile :: FilePath -> IOMode -> m (Handle m)
 
-    -- Like B.readFile
+    -- 'B.readFile'
     readFileBS :: FilePath -> m B.ByteString
 
-    -- | Save the provided ByteString to the named file, if the file does
-    -- not already exist. If it does exist, this is a no-op.
+    -- This is the one function that doesn't map almost directly  to an
+    -- existant library function in IO. Save the provided ByteString to
+    -- the named file, if the file does not already exist. If it does
+    -- exist, this is a no-op.
     saveFile :: FilePath -> B.ByteString -> m ()
 
     listDirectory :: FilePath -> m [FilePath]
     createDirectoryIfMissing :: Bool -> FilePath -> m ()
 
+    -- Like 'sinkFileBS', except that it doesn't need to run in a MonadResource,
+    -- and accepts a file handle, rather than a path.
+    --
+    -- This makes things easier to work with since runConduitRes requires IO
+    -- at the base of the stack.
     fsSinkFileBS :: Handle m -> Consumer B.ByteString m ()
+
+    -- 'runConduit'
     fsRunConduit :: ConduitM () Void m a -> m a
 
     getSymbolicLinkStatus :: (FileStatus m ~ status) => FilePath -> m status
     readSymbolicLink :: FilePath -> m FilePath
 
+    -- These are like their counterparts from the unix library, except that
+    -- the return value is monadic. These shouldn't actually have any effects;
+    -- I was getting errors from the type checker about ambiguity when I just
+    -- had them as :: FileStatus m -> Bool, and couldn't sort it out :/
     isRegularFile :: FileStatus m -> m Bool
     isDirectory :: FileStatus m -> m Bool
     isSymbolicLink :: FileStatus m -> m Bool
 
 instance MonadFileSystem IO where
+    -- trivial aliases:
     type Handle IO = IO.Handle
     type FileStatus IO = P.FileStatus
-
     hGetBS = B.hGet
     hClose = IO.hClose
     openBinaryFile = IO.openBinaryFile
     readFileBS = B.readFile
+    getSymbolicLinkStatus = P.getSymbolicLinkStatus
+    readSymbolicLink = P.readSymbolicLink
+    listDirectory = Dir.listDirectory
+    createDirectoryIfMissing = Dir.createDirectoryIfMissing
+    fsRunConduit = runConduit
+
+    -- just add pure. See the comments in the type class.
+    isRegularFile = pure . P.isRegularFile
+    isDirectory = pure . P.isDirectory
+    isSymbolicLink = pure . P.isSymbolicLink
+
+    fsSinkFileBS h = mapM_C (B.hPut h)
+
     saveFile filename bytes =
         bracket
             createExclusive
@@ -174,15 +200,6 @@ instance MonadFileSystem IO where
             P.WriteOnly
             (Just 0o600)
             P.defaultFileFlags { P.exclusive = True }
-    fsSinkFileBS h = mapM_C (B.hPut h)
-    getSymbolicLinkStatus = P.getSymbolicLinkStatus
-    readSymbolicLink = P.readSymbolicLink
-    isRegularFile = pure . P.isRegularFile
-    isDirectory = pure . P.isDirectory
-    isSymbolicLink = pure . P.isSymbolicLink
-    listDirectory = Dir.listDirectory
-    createDirectoryIfMissing = Dir.createDirectoryIfMissing
-    fsRunConduit = runConduit
 
 -- | @'blockPath' store digest@ is the file name in which the block with
 -- sha256 hash @digest@ should be saved within the given store.
