0618f5b20ab1353946e9c9ea62a4ed2029ad9116
Ian Denhardt <ian@zenhack.net>
2017-12-14 20:11:07 -0500

    FakeFS: Implement openBinaryFile.
    ...and some prereq changes.

--- tests/FakeFS.hs
+++ tests/FakeFS.hs
@@ -10,6 +10,7 @@ import Control.Monad.Catch.Pure (CatchT)
 import Control.Monad.State      (State, get, modify, put)
 import Data.Hashable            (Hashable)
 import Data.Int                 (Int64)
+import System.IO                (IOMode)
 import System.IO.Error
     (doesNotExistErrorType, illegalOperationErrorType, mkIOError)
 
@@ -28,12 +29,17 @@ type Offset = Int64
 newtype INodeRef = INodeRef Int
     deriving(Show, Eq, Ord, Hashable)
 
-data FakeFile = FakeFile !Offset !INodeRef
+data FakeFile = FakeFile
+    { mode     :: !IOMode
+    , offset   :: !Offset
+    , inodeRef :: !INodeRef
+    }
 
 data FsState = FsState
-    { handles  :: M.HashMap FakeHandle FakeFile
-    , rootNode ::  INodeRef
-    , inodes   :: M.HashMap INodeRef INode
+    { handles    :: M.HashMap FakeHandle FakeFile
+    , rootNode   :: !INodeRef
+    , inodes     :: M.HashMap INodeRef INode
+    , nextHandle :: !Int
     }
 
 data INode
@@ -65,17 +71,23 @@ parsePath path = normalize (split '/' path)
     normalize (x:xs)      = x:normalize xs
     normalize []          = []
 
-inodeByPath :: FilePath -> FakeFS INode
+newHandle :: FakeFS FakeHandle
+newHandle = FakeFS $ do
+    fs@FsState{..} <- get
+    put fs { nextHandle = nextHandle + 1}
+    return $ FakeHandle nextHandle
+
+inodeByPath :: FilePath -> FakeFS (INodeRef, INode)
 inodeByPath path = FakeFS $ do
     FsState{..} <- get
     go rootNode (parsePath path)
   where
-    go :: INodeRef -> [String] -> CatchT (State FsState) INode
+    go :: INodeRef -> [String] -> CatchT (State FsState) (INodeRef, INode)
     go iref pathParts = do
         FsState{..} <- get
         case (M.lookup iref inodes, pathParts) of
             (Just inode, []) ->
-                return inode
+                return (iref, inode)
             (Just (IDirectory dir), p:ps) -> case M.lookup p dir of
                 Just iref' ->
                     go iref' ps
@@ -91,30 +103,45 @@ instance MonadFileSystem FakeFS where
     type FileStatus FakeFS = FakeFileStatus
 
     hGetBS h len = FakeFS $ do
+        -- TODO: check that the mode of h is readable.
         fs@FsState{..} <- get
         case M.lookup h handles of
             Nothing ->
                 throwM $ mkIOError illegalOperationErrorType "Invalid Handle" Nothing Nothing
-            Just (FakeFile off iref) -> case M.lookup iref inodes of
+            Just (file@FakeFile{..}) -> case M.lookup inodeRef inodes of
                 Just (IRegFile bytes) -> do
-                    let result = LBS.take (fromIntegral len) $ LBS.drop off bytes
-                    let off' = max (off + fromIntegral len) (LBS.length bytes)
-                    put fs { handles = M.insert h (FakeFile off' iref) handles }
+                    let result = LBS.take (fromIntegral len) $ LBS.drop offset bytes
+                    let off' = max (offset + fromIntegral len) (LBS.length bytes)
+                    put fs { handles = M.insert h (file { offset = off'
+                                                        , inodeRef = inodeRef
+                                                        }) handles
+                           }
                     return $ LBS.toStrict result
                 _ -> error "BUG: Handle is valid but inode does not exist."
 
     hClose h = FakeFS $ modify $
         \fs -> fs { handles = M.delete h (handles fs) }
 
+    openBinaryFile path mode = do
+        (iref, _) <- inodeByPath path
+        h <- newHandle
+        let file = FakeFile { mode = mode
+                            , offset = 0
+                            , inodeRef = iref
+                            }
+        FakeFS $ modify (\fs@FsState{..} ->
+            fs { handles = M.insert h file handles })
+        return h
+
     getSymbolicLinkStatus path = do
-        inode <- inodeByPath path
+        (_, inode) <- inodeByPath path
         return $ case inode of
             IRegFile _   -> nilStatus { _isRegularFile = True }
             IDirectory _ -> nilStatus { _isDirectory = True }
             ISymLink _   -> nilStatus { _isSymbolicLink = True }
 
     readSymbolicLink path = do
-        inode <- inodeByPath path
+        (_, inode) <- inodeByPath path
         case inode of
             ISymLink target ->
                 return target
