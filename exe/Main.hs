a6708c8a24ada19755359785e97aab473f2df6e7
Ian Denhardt <ian@zenhack.net>
2017-12-23 15:16:07 -0500

    Use optparse-applicative for the cli

--- exe/Main.hs
+++ exe/Main.hs
@@ -1,11 +1,3 @@
 module Main (main) where
 
-import Cli
-import System.Environment (getArgs)
-
-main :: IO ()
-main = do
-    args <- getArgs
-    case args of
-        [storePath, "tags"] -> runCommand storePath Tags
-        _                   -> putStrLn "Unknown command"
+import Cli (main)
--- lib/Cli.hs
+++ lib/Cli.hs
@@ -1,7 +1,9 @@
 module Cli where
 
 import DedupBackupNG
+import Options.Applicative
 
+import Data.Monoid      (mempty, (<>))
 import System.Directory (listDirectory)
 
 data Command
@@ -9,7 +11,25 @@ data Command
     | Save FilePath
     deriving(Show, Read, Eq)
 
+cmdParser :: Parser (FilePath, Command)
+cmdParser = (,)
+    <$> strOption
+        ( short 's'
+        <> long "store"
+        <> metavar "STORE"
+        <> help "path to the store"
+        )
+    <*> hsubparser
+        ( command "tags"
+            (info (pure Tags) (progDesc "List tags of snapshots in the store"))
+        )
+
 runCommand :: FilePath -> Command -> IO ()
 runCommand storePath Tags = do
     contents <- listDirectory (storePath ++ "/tags")
     mapM_ putStrLn contents
+
+main :: IO ()
+main = do
+    (storePath, cmd) <- execParser $ info cmdParser mempty
+    runCommand storePath cmd
