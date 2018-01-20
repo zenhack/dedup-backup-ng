c9a78ac1eeaed2337891633f4cbc832244459ef7
Ian Denhardt <ian@zenhack.net>
2018-01-19 21:44:35 -0500

    More useful test output.
    It actually shows us a diff now (along with some info about it).
    
    This pointed me at what I think is the source of the problem: we're
    truncating the blob file on subsequent opens. Shouldn't be too hard to
    fix.
    
    The CBOR error was a red herring -- just me fat-fingering and typing
    Metadata (from DDB.Types) instead of MetaData (from NewStore). I ought
    to rename one of those...

--- tests/TestMain.hs
+++ tests/TestMain.hs
@@ -5,7 +5,8 @@ import DDB.NewStore
 import DDB.SimpleStore
 import DDB.Types
 
-import Control.Monad                        (replicateM)
+import Control.Monad                        (replicateM, when)
+import Data.Algorithm.Diff                  (Diff(..), getGroupedDiff)
 import Data.Proxy                           (Proxy(..))
 import System.Unix.Directory                (withTemporaryDirectory)
 import Test.Framework                       (defaultMain)
@@ -17,9 +18,16 @@ import qualified Data.ByteString as B
 
 -- Generate a bytestring up to 4 MiB in size. This is big enough to test
 -- deep-ish trees without being prohibitive.
+--
+-- TODO: we are actually currently using a block size that is much smaller,
+-- because if we show diffs on failure a large block size causes us to run out of
+-- memory (the diff algorithm uses quadratic space). Probably the best solution
+-- is to make the block size tunable, so that we can have a smaller value during
+-- test.
 genBlob :: Gen B.ByteString
 genBlob = do
-    numBytes <- choose (0 :: Int, 4 * 1024 * 1024)
+--    numBytes <- choose (0 :: Int, 4 * 1024 * 1024)
+    numBytes <- choose (0 :: Int, 1024)
     B.pack <$> replicateM numBytes arbitrary
 
 saveRestoreBlob :: Store a => Proxy a -> FilePath -> B.ByteString -> IO Bool
@@ -36,10 +44,25 @@ saveRestoreBlob proxy path blob = do
     old <- B.readFile oldPath
     new <- B.readFile newPath
     closeStore store'
+    when (old /= new) $ do
+        putStrLn "restored data is corrupted."
+        putStrLn ""
+        let diff = getGroupedDiff (B.unpack old) (B.unpack new)
+        putStrLn $ "Number of chunks in diff : " ++ show (length diff)
+        putStrLn $ "Chunk sizes : " ++ show (map chunkSize diff)
+        putStrLn "Diff:"
+        putStrLn ""
+        print diff
     return (old == new)
   where
     asIOType :: Proxy a -> IO a -> IO a
     asIOType _ x = x
+
+    -- | Determine the size of a chunk
+    chunkSize (First x)  = length x
+    chunkSize (Second x) = length x
+    -- XXX: this is a bit wrong; we're ignoring the second part.
+    chunkSize (Both x _) = length x
 
 prop_saveRestoreBlob :: Store a => Proxy a -> Property
 prop_saveRestoreBlob proxy = monadicIO $ do
